08ce0b9035194272c0efc08bda293f813d0c9d52
Carlos Botelho <70547443+car2100@users.noreply.github.com>
2025-03-11 17:22:25 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package area
+
+import "math"
+
+const Pi = 3.1415
+
+// circ calcula a area da circunferencia
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+// Rect calcula area de retangulo
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+// não é visivel
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
